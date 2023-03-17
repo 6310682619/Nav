acf7bb50425b6c48a25ae43b34f38979d3ff193b
นางสาวเขมิกา วีรกุลวัฒนา <6310682619@eng.tu>
2023-03-17 12:18:08 +0700

    nav example

--- MainActivity.kt
+++ MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -19,6 +24,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.navexample.ui.theme.NavExampleTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 //sealed class Screen(val route: String) {
 //    object Home: Screen("home")
@@ -80,7 +86,8 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
-        Column() {
+        Column(Modifier.fillMaxWidth().padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,) {
             Button(onClick = { navController.navigate("greet/World") }) {
                 Text("World")
             }
